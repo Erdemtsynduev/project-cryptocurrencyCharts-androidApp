c2e8f0c8e599da03cc29d6bc982bdcc7bae56f76
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-07-26 22:47:55 +0300

    refactor: Rename class api

--- app/src/main/java/com/erdemtsynduev/profitcoin/network/api/CoinMarketCapApi.java
+++ app/src/main/java/com/erdemtsynduev/profitcoin/network/api/CoinMarketCapApi.java
@@ -5,7 +5,7 @@ import com.erdemtsynduev.profitcoin.network.model.ResponseItem;
 import retrofit2.http.GET;
 import rx.Observable;
 
-public interface RetrofitGetInterface {
+public interface CoinMarketCapApi {
 
     @GET("v1/ticker/")
     Observable<ResponseItem> getCryptocurrency();
