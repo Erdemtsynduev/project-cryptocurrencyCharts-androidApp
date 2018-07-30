c13c50a175722b050ca2e7f3e9e2c2c8f0c4e478
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-07-30 18:57:36 +0300

    feat: Add new module to component

--- app/src/main/java/com/erdemtsynduev/profitcoin/di/AppComponent.java
+++ app/src/main/java/com/erdemtsynduev/profitcoin/di/AppComponent.java
@@ -3,7 +3,9 @@ package com.erdemtsynduev.profitcoin.di;
 import android.content.Context;
 
 import com.erdemtsynduev.profitcoin.di.Modules.BusModule;
+import com.erdemtsynduev.profitcoin.di.Modules.CoinMarketCapModule;
 import com.erdemtsynduev.profitcoin.di.Modules.ContextModule;
+import com.erdemtsynduev.profitcoin.network.CoinMarketCapService;
 import com.squareup.otto.Bus;
 
 import javax.inject.Singleton;
@@ -11,9 +13,11 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {ContextModule.class, BusModule.class})
+@Component(modules = {ContextModule.class, BusModule.class, CoinMarketCapModule.class})
 public interface AppComponent {
     Context getContext();
+
     Bus getBus();
 
+    CoinMarketCapService getCoinMarketCapService();
 }
