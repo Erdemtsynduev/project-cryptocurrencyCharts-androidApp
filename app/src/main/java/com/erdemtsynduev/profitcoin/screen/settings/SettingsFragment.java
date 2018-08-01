5451baf39faadbffc97762fe094aabd593ed968b
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-08-01 20:00:06 +0300

    fix: Change method after crush

--- app/src/main/java/com/erdemtsynduev/profitcoin/screen/settings/SettingsFragment.java
+++ app/src/main/java/com/erdemtsynduev/profitcoin/screen/settings/SettingsFragment.java
@@ -3,57 +3,46 @@ package com.erdemtsynduev.profitcoin.screen.settings;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.erdemtsynduev.profitcoin.R;
 
 import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
-import static com.erdemtsynduev.profitcoin.utils.Utils.checkNotNull;
+public class SettingsFragment extends MvpAppCompatFragment implements SettingsView {
 
-public class SettingsFragment extends Fragment implements SettingsContract.View {
+    @InjectPresenter
+    SettingsPresenter mSettingsPresenter;
 
-    private SettingsContract.Presenter mPresenter;
-
-    private Unbinder unbinder;
-
-    public static SettingsFragment newInstance() {
+    public static SettingsFragment getInstance() {
         return new SettingsFragment();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mPresenter.start();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        unbinder = ButterKnife.bind(this, view);
-        return view;
-    }
-
-
-    @Override
-    public void setPresenter(@NonNull SettingsContract.Presenter presenter) {
-        mPresenter = checkNotNull(presenter);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void showEmptyLogIn() {
+
+    }
+
+    @Override
+    public void showLogIn() {
+
     }
 }
