c6f9cf0f59c92ea5c8796003f8cc89b743e2c591
Erdem Tsynduev <erdemtsynduev@mail.ru>
2018-08-17 13:41:55 +0300

    feat: Add okhttp library and used that

--- app/src/main/java/com/erdemtsynduev/profitcoin/di/Modules/RetrofitModule.java
+++ app/src/main/java/com/erdemtsynduev/profitcoin/di/Modules/RetrofitModule.java
@@ -1,18 +1,14 @@
 package com.erdemtsynduev.profitcoin.di.Modules;
 
 import com.erdemtsynduev.profitcoin.BuildConfig;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.FieldNamingStrategy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.lang.reflect.Field;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -30,35 +26,20 @@ public class RetrofitModule {
     @Provides
     @Singleton
     public Retrofit.Builder provideRetrofitBuilder(Converter.Factory converterFactory) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HttpLoggingInterceptor().setLevel((BuildConfig.DEBUG)
+                        ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE))
+                .build();
+
         return new Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .addConverterFactory(converterFactory);
+                .client(okHttpClient)
+                .addConverterFactory(converterFactory)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()));
     }
 
     @Provides
     @Singleton
-    public Converter.Factory provideConverterFactory(Gson gson) {
-        return GsonConverterFactory.create(gson);
-    }
-
-    @Provides
-    @Singleton
-    Gson provideGson() {
-        return new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setFieldNamingStrategy(new CustomFieldNamingPolicy())
-                .setPrettyPrinting()
-                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-                .serializeNulls()
-                .create();
-    }
-
-    private static class CustomFieldNamingPolicy implements FieldNamingStrategy {
-        @Override
-        public String translateName(Field field) {
-            String name = FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES.translateName(field);
-            name = name.substring(2, name.length()).toLowerCase();
-            return name;
-        }
+    public Converter.Factory provideConverterFactory() {
+        return GsonConverterFactory.create();
     }
 }
