e677dcbfc6db295dbc373acbd819fc2f17465dd4
Erdem Tsynduev <erdemtsynduev@gmail.com>
2020-09-08 17:23:52 +0300

    feat: Add api key

--- app/src/main/java/com/erdemtsynduev/profitcoin/utils/ApiKeyUtils.java
+++ app/src/main/java/com/erdemtsynduev/profitcoin/utils/ApiKeyUtils.java
@@ -4,7 +4,7 @@ public class ApiKeyUtils {
     private static final String API_KEY = "apikey";
 
     public static String getApiKey() {
-        return PrefUtils.getPrefs().getString(API_KEY, "");
+        return PrefUtils.getPrefs().getString(API_KEY, "97b8ef41-3c54-4c22-98ee-16c807029099");
     }
 
     public static void setApiKey(String token) {
